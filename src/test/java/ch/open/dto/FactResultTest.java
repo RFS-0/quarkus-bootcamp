63b7ee35ec5f55b62c825d09d7626e2c18dcabe7
Schenker, Remo <remo.schenker@zuehlke.com>
2021-08-25 18:28:07 +0200

    2.3 Add test coverage with Jacoco
    Configures the Jacoco plugin to work with Unit and Quarkus tests.

---
+++ src/test/java/ch/open/dto/FactResultTest.java
@@ -0,0 +1,16 @@
+package ch.open.dto;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FactResultTest {
+
+
+
+}
